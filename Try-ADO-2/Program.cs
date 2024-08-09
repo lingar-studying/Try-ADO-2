c046b45a4b16aa0c0b6f2b8e0c597d03fe4b4acc
lingar <yimprogramming@gmail.com>
2024-08-09 13:30:43 +0300

    Basics of ADO

--- Try-ADO-2/Program.cs
+++ Try-ADO-2/Program.cs
@@ -8,6 +8,16 @@ using System.Text;
 using System.Threading.Tasks;
 //using QC = Microsoft.Data.SqlClient;
 
+/**
+ * 
+ * To check:
+ * 
+ * insert object:
+ * https://learn.microsoft.com/en-us/dotnet/framework/data/adonet/sql/linq/how-to-insert-rows-into-the-database
+https://stackoverflow.com/questions/13573380/inserting-values-into-a-sql-server-database-using-ado-net-via-c-sharp
+https://learn.microsoft.com/en-us/dotnet/framework/data/adonet/updating-data-in-a-data-source
+
+*/
 namespace Try_ADO_2
 {
     internal class Program
@@ -75,8 +85,10 @@ namespace Try_ADO_2
             Console.WriteLine("Connected successfully.");
 
             Console.WriteLine("State = " + con.State);
-
+            Console.WriteLine("initial cars: ");
             PrintCars();
+
+            AddCar(5556666, "Ford", "fiesta");
 
 
             Console.WriteLine("Press any key to finish...");
@@ -110,6 +122,82 @@ namespace Try_ADO_2
             }
             reader.Close();
         }
+
+        public static void CreateCarRow(int id, string carName,
+            string carBrand)
+        {
+            ////assuming that connection is open
+
+            //SqlCommand command2 = new SqlCommand("SELECT * FROM Cars;",con);
+            string query = "SELECT * FROM cars;";
+            SqlCommand command = new SqlCommand(query, con);
+
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
+                        reader.GetString(1));
+                }
+            }
+            else
+            {
+                Console.WriteLine("No rows found.");
+            }
+            reader.Close();
+        }
+
+        public static void AddCar(int id, string carName, string brand)
+        {
+            string query = "INSERT INTO cars (car_id, CarName, CARBrand) VALUES (@Id, @CarName, @Brand)";
+            SqlCommand command = new SqlCommand(query, con);
+
+            command.Parameters.AddWithValue("@Id", id);
+            command.Parameters.AddWithValue("@CarName", carName);
+            command.Parameters.AddWithValue("@Brand", brand);
+
+            try
+            {
+                int rowsAffected = command.ExecuteNonQuery();
+                Console.WriteLine($"Rows inserted: {rowsAffected}");
+
+                PrintCars();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+
+        //private void InsertData(string connectionString, string firstName, string lastname, string username, string password
+        //                int Age, string gender, string contact)
+        //{
+        //    // define INSERT query with parameters
+        //    string query = "INSERT INTO dbo.regist (FirstName, Lastname, Username, Password, Age, Gender,Contact) " +
+        //                   "VALUES (@FirstName, @Lastname, @Username, @Password, @Age, @Gender, @Contact) ";
+
+        //    // create connection and command
+        //    using (SqlConnection cn = new SqlConnection(connectionString))
+        //    using (SqlCommand cmd = new SqlCommand(query, cn))
+        //    {
+        //        // define parameters and their values
+        //        cmd.Parameters.Add("@FirstName", SqlDbType.VarChar, 50).Value = firstName;
+        //        cmd.Parameters.Add("@Lastname", SqlDbType.VarChar, 50).Value = lastName;
+        //        cmd.Parameters.Add("@Username", SqlDbType.VarChar, 50).Value = userName;
+        //        cmd.Parameters.Add("@Password", SqlDbType.VarChar, 50).Value = password;
+        //        cmd.Parameters.Add("@Age", SqlDbType.Int).Value = age;
+        //        cmd.Parameters.Add("@Gender", SqlDbType.VarChar, 50).Value = gender;
+        //        cmd.Parameters.Add("@Contact", SqlDbType.VarChar, 50).Value = contact;
+
+        //        // open connection, execute INSERT, close connection
+        //        cn.Open();
+        //        cmd.ExecuteNonQuery();
+        //        cn.Close();
+        //    }
+        //}
 
     }
 }
